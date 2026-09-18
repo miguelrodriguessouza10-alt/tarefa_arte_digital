ddd4cf894e6b3394b81be867973722cf0660e075
miguelrodriguessouza10-alt <miguel.rodrigues.souza10@escola.pr.gov.br>
2026-09-18 10:58:25 -0300

    atualizei o codigo

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+const botoesCurtir = document.querySelectorAII(.curtir);
+botoesCurtir.forEach(fucntion(botaoCurtir){
+    let curtiu = false;
+    botaoCurtir.addEventlistener("click",curtir);
+function curtir (){
+     const contador=botaoCurtir.querySelector("span");
+     if(curtiu === false){
+            contaor.textContent++;
+            curtiu = true;}
+            else{
+                contador.textContent--;
+                curtiu=false;
+        }
+    }
+});
